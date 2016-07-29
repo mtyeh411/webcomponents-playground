defe192677bf3213ee2038917eadc95f62b50331
Matt Yeh <dev.mtyeh411@gmail.com>
2016-07-28 22:42:16 -0400

    used webcomponents as dependency

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var PORT = 3002;
+
 app.use(express.static(__dirname));
+app.use('/scripts', express.static(__dirname + '/node_modules/webcomponents.js/'));
 
 app.get('/', function(req, res) {
  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3002);
+app.listen(PORT, function() {
+  console.log('Started server...\nListening on port ' + PORT + '...');
+});
