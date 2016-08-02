98761dcd5bef0b252a597c45d7fc0aeeddad5a38
Matt Yeh <dev.mtyeh411@gmail.com>
2016-08-02 07:35:05 -0400

    example using React

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var PORT = 3002;
 app.use(express.static(__dirname));
 app.use('/scripts', express.static(__dirname + '/node_modules/webcomponents.js/'));
 app.use('/scripts', express.static(__dirname + '/node_modules/angular/'));
+app.use('/scripts', express.static(__dirname + '/node_modules/react/dist'));
+app.use('/scripts', express.static(__dirname + '/node_modules/react-dom/dist'));
 
 app.get('/', function(req, res) {
  res.sendFile(path.join(__dirname + '/index.html'));
